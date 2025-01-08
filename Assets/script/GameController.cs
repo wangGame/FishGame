1b4025d6540fea87410b8a742d28a40d8f56b9b1
kangwang <2818815189@qq.com>
2025-01-08 22:58:33 +0800

    完善

--- Assets/script/GameController.cs
+++ Assets/script/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
@@ -9,6 +10,17 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI costText;  
     public GameObject[] gunGos;
+
+    public Transform bulletHolder;
+
+    public int lv;
+
+    public GameObject[] bullet1Gos;
+    public GameObject[] bullet2Gos;
+    public GameObject[] bullet3Gos;
+    public GameObject[] bullet4Gos;
+    public GameObject[] bullet5Gos;
+
     private int costIndex =0;
     private int[] oneShootCosts = {
         5,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000
@@ -36,6 +48,7 @@ public class GameController : MonoBehaviour
 
     private void Update()
     {
+        Fire();
         ChangeBulletCost();
     }
 
@@ -50,7 +63,30 @@ public class GameController : MonoBehaviour
             //shang
             OnButtonPDown();
         }
+    }
 
-
+    void Fire() {
+        GameObject[] useBullets = bullet4Gos;
+        int bulletIndex;
+        if (Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject() == false) {
+            //×ó¼ü
+            switch (costIndex / 4) { 
+                case 0:useBullets = bullet1Gos; break;
+                case 1:useBullets = bullet2Gos; break;
+                case 2:useBullets = bullet3Gos; break;
+                case 3:useBullets = bullet4Gos; break;
+                case 4: useBullets = bullet5Gos; break;
+            }
+            bulletIndex = (lv % 10 >= 9) ? 9 : lv%10;
+            GameObject bullet = Instantiate(useBullets[bulletIndex]);
+            bullet.transform.SetParent(bulletHolder, false);
+            bullet.transform.position =
+                gunGos[costIndex / 4].transform.Find("firpos").transform.position;
+            bullet.transform.rotation =
+                gunGos[costIndex / 4].transform.Find("firpos").transform.rotation;
+            Ef_AutoMove ef = bullet.AddComponent<Ef_AutoMove>();
+            ef.speed = 10f;
+            ef.dir = Vector3.up;
+        }
     }
 }  
